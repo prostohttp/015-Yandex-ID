8552b1e86632c1650d42e57819f95b7b126086b0
Morozov Mikhail <memphis.sail@gmail.com>
2023-12-11 21:22:17 +1000

    add cors

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,19 +1,9 @@
 <template>
   <div>
     <h1>Login</h1>
-    <a href="http://localhost:3000/login">Войти с yandex</a>
+    <a href="/api/login">Войти с yandex</a>
   </div>
 </template>
 
 <script setup>
-// const yandexAuthHandler = async () => {
-//   await fetch("http://localhost:3000/login", {
-//     mode: "cors",
-//     credentials: "include",
-//     headers: {
-//       'Access-Control-Allow-Origin': '*',
-//       'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-//     }
-//   })
-// }
 </script>
